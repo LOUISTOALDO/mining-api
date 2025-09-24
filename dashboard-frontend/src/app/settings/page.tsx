701dff74beec2df2881a0c9483932ab9b5e119dc
LOUISTOALDO <toaldolouis@gmail.com>
2025-09-24 10:19:10 +1000

    Delete dashboard-frontend directory

--- dashboard-frontend/src/app/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserRoles } from "@/components/user-management/user-roles"
-
-export default function SettingsPage() {
-  return (
-    <div className="space-y-6">
-      <UserRoles />
-    </div>
-  )
-}
